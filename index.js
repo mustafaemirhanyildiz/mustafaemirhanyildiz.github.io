20560a816947e1b0c867532802be6875b68da6f9
Mustafa Emirhan Yıldız <92929045+mustafaemirhanyildiz@users.noreply.github.com>
2023-03-30 11:18:06 +0300

    contact form

--- index.js
+++ index.js
@@ -49,3 +49,12 @@ var Text = function(el, toRotate, period) {
       }
     }
   }; 
+
+
+
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
